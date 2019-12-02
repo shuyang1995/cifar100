6a8bef653d1b4812a253e46013771e262123e9ac
ShuYang <shuyang1995@ucsb.edu>
2019-12-02 15:39:23 -0800

    normalize

--- Train.py
+++ Train.py
@@ -13,6 +13,8 @@ best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 CIFAR100_TRAIN_MEAN = (0.5070751592371323, 0.48654887331495095, 0.4409178433670343)
 CIFAR100_TRAIN_STD = (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)
+#CIFAR100_TEST_MEAN = (0.5088964127604166, 0.48739301317401956, 0.44194221124387256)
+#CIFAR100_TEST_STD = (0.2682515741720801, 0.2573637364478126, 0.2770957707973042)
 #loaddata -- cifar100
 print('==> Preparing data..')
 
@@ -26,7 +28,7 @@ transform_train = transforms.Compose([
 
 transform_test = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+    transforms.Normalize(CIFAR100_TRAIN_MEAN,CIFAR100_TRAIN_STD),
 ])
 
 trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform_train)
