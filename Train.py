b7088224438832d2ec17b1daf5a3ceff661addea
ShuYang <shuyang1995@ucsb.edu>
2019-12-02 15:35:31 -0800

    normalize

--- Train.py
+++ Train.py
@@ -11,7 +11,8 @@ import os
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
-
+CIFAR100_TRAIN_MEAN = (0.5070751592371323, 0.48654887331495095, 0.4409178433670343)
+CIFAR100_TRAIN_STD = (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)
 #loaddata -- cifar100
 print('==> Preparing data..')
 
@@ -20,7 +21,7 @@ transform_train = transforms.Compose([
     transforms.RandomHorizontalFlip(),
     transforms.RandomRotation(15),
     transforms.ToTensor(),
-    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+    transforms.Normalize(CIFAR100_TRAIN_MEAN,CIFAR100_TRAIN_STD),
 ])
 
 transform_test = transforms.Compose([
