ca8952a5c08c57d99b3a032a5502adc0cd7eac37
ShuYang <shuyang1995@ucsb.edu>
2019-12-02 16:16:33 -0800

    commit old train

--- Train.py
+++ Train.py
@@ -53,28 +53,28 @@ iter_per_epoch = len(trainloader)
 warmup_scheduler = WarmUpLR(optimizer, iter_per_epoch * 1)
 
 def train(epoch):
-'''
-    print('\nEpoch: %d' % epoch)
-    if epoch >= 2:
-        train_scheduler.step(epoch)
-    net.train()
-    train_loss = 0
-    correct = 0
-    total = 0
-    for batch_idx, (inputs, targets) in enumerate(trainloader):
-        inputs, targets = inputs.to(device), targets.to(device)
-        optimizer.zero_grad()
-        outputs = net(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
-        train_loss += loss.item()
-        _, predicted = outputs.max(1)
-        total += targets.size(0)
-        correct += predicted.eq(targets).sum().item()
-        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-            % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
-'''
+
+#    print('\nEpoch: %d' % epoch)
+#    if epoch >= 2:
+#        train_scheduler.step(epoch)
+#    net.train()
+#    train_loss = 0
+#    correct = 0
+#    total = 0
+#    for batch_idx, (inputs, targets) in enumerate(trainloader):
+#        inputs, targets = inputs.to(device), targets.to(device)
+#        optimizer.zero_grad()
+#        outputs = net(inputs)
+#        loss = criterion(outputs, targets)
+#        loss.backward()
+#        optimizer.step()
+#        train_loss += loss.item()
+#        _, predicted = outputs.max(1)
+#        total += targets.size(0)
+#        correct += predicted.eq(targets).sum().item()
+#        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+#            % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
     net.train()
     train_loss = 0
     correct = 0
